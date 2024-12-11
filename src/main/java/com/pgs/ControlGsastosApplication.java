da769d047a020e77d0943f3eeeb63af0bbe07877
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-12-11 19:08:12 +0100

    First commit

---
+++ src/main/java/com/pgs/ControlGsastosApplication.java
@@ -0,0 +1,13 @@
+package com.pgs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlGsastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlGsastosApplication.class, args);
+	}
+
+}
