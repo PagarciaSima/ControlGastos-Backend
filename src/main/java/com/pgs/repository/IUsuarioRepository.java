9cd5e4715337101e9ad550a33e9e03f1bd4c3b10
pagarciaSim <pablo.garciasimavilla@gmail.com>
2024-12-16 12:23:43 +0100

    FindByCorreo & findByCorreoEstadosID (UsuarioModel)

--- src/main/java/com/pgs/repository/IUsuarioRepository.java
+++ src/main/java/com/pgs/repository/IUsuarioRepository.java
@@ -1,9 +1,14 @@
 package com.pgs.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.pgs.model.EstadoModel;
 import com.pgs.model.UsuarioModel;
 
 public interface IUsuarioRepository extends JpaRepository<UsuarioModel, Long>{
 
+	UsuarioModel findByCorreo(String correo);
+	Optional<UsuarioModel> findByCorreoAndEstadosId(String correo, EstadoModel estado);
 }
